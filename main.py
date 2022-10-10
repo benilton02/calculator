fd6a398b0173de7a887ef16b401ca144d2745835
benilton02 <bsa.ele@uea.edu.br>
2022-10-10 10:05:50 -0400

    simple calculator of two numbers

---
+++ main.py
@@ -0,0 +1,43 @@
+class Factors():
+        
+    def __init__(self):
+        self.x = int(input('Enter only two factors: '))
+        self.y = int(input('Enter only two factors: '))        
+
+class Calculator(Factors):
+    
+        
+    def addition(self):
+        return self.x + self.y
+
+
+    def subtraction(self):
+        return self.x - self.y
+        
+    
+    def division(self):
+        return self.x / self.y
+        
+    
+    def multiplication(self):
+        return self.x * self.y
+
+
+    def show(self):
+        x=self.x
+        y=self.y
+    
+        addition = calculator.addition()
+        subtraction = calculator.subtraction()
+        division = calculator.division()
+        multiplication = calculator.multiplication()
+    
+        print(f'\n{x} + {y} = {addition}\n')    
+        print(f'{x} - {y} = {subtraction}\n')
+        print(f'{x} / {y} = {division}\n')
+        print(f'{x} * {y} = {multiplication}\n')
+    
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    calculator.show()
